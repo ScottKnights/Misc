84b38a44ce41ab1462470c2550c9301d898992fc
Scott Knights <scott.knights@area9.com.au>
2021-11-12 15:22:09 +0930

    Fixed indenting. Comment changes.

--- bobify-text.ps1
+++ bobify-text.ps1
@@ -1,6 +1,7 @@
-# A commenter on a forum I post to randomly capitalizes words in his posts for no discernable reasons.
+# A commenter on a forum I post to appears to randomly capitalize words in his posts for no discernable reason.
 # I decided to try write a function to convert text to give the "Bob Experience".
-#
+# Hmmmm. According to ScriptAnalyzer "bobify is an unapproved verb".
+
 # Usage - bobify-text "Some text you want Bobifying" -bobfactor (0 - 5, default 1)
 # Usage - bobify-text $text -bobfactor (0 - 5, default 1) # $text is a string variable containing text to bobify
 
@@ -13,21 +14,21 @@ function bobify-text {
 	[int] $bobfactor=1
 )
 
-# Split the text into an array of seperate words
-$array = $text -split " "
+	# Split the text into an array of seperate words
+	$array = $text -split " "
 
-# Each word has a Bobfactor in 5 chance of being 'Bobbed'. 0 = No bobification, 5 = Max bobification (actually anything >4)
-$newArray = @()
-foreach ($word in $array) {
-	$random = Get-Random 5
-	if ($random -lt $bobfactor) {
-		$word = $word.ToUpper()
+	# Each word has a Bobfactor in 5 chance of being 'Bobbed'. 0 = No bobification, 5 = Max bobification (actually anything >4)
+	$newArray = @()
+	foreach ($word in $array) {
+		$random = Get-Random 5
+		if ($random -lt $bobfactor) {
+			$word = $word.ToUpper()
+		}
+		$newArray += $word
 	}
-	$newArray += $word
-}
 
-# Rejoin and return the text array. Capitalise the first letter.
-$outtext = $newArray -join ' '
-$outtext = $outtext.substring(0,1).toupper()+$outtext.substring(1)
-return $outtext
+	# Rejoin and return the text array. Capitalise the first letter.
+	$outtext = $newArray -join ' '
+	$outtext = $outtext.substring(0,1).toupper()+$outtext.substring(1)
+	return $outtext
 }
